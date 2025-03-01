d55fb19681520b20f9e19eb7a3a168c551742fb2
def <153857992+def0001@users.noreply.github.com>
2025-03-01 21:20:59 +0200

    Create text_dublicator.cs
    Небольшой скрипт, написанный на C#, с помощью которого можно дублировать текст.

---
+++ text_dublicator.cs
@@ -0,0 +1,40 @@
+// Прошло всего несколько дней с того момента, как я начал изучать C#. Пока что, это всё что я запомнил.
+using System;
+
+namespace project
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите слово: ");
+            string word = Console.ReadLine();
+            if (word == null)
+            {
+                word = "null";
+            }
+
+            while (true)
+            {
+                Console.Write("Введите число повторений (не больше 32,766 и не меньше 2): ");
+                short number = Convert.ToInt16(Console.ReadLine());
+                if (number >= 32767 && number <= 1) // Предохранитель от краша
+                {
+                    number = 1;
+                    Console.WriteLine("Блять долбаи давай по новой");
+                    Console.Write("Введите число повторений (не больше 32,767): ");
+                    number = Convert.ToInt16(Console.ReadLine());
+                }
+                else
+                {
+                    for (short i = 0; i <= number; i++)
+                    {
+                        Console.WriteLine($"{i}) {word}");
+                    }
+                    break;
+                }
+
+            }
+        }
+    }
+}
