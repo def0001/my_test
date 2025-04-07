17b3415cdb9b12e010807718b7d25deadcf24354
def <153857992+def0001@users.noreply.github.com>
2025-04-07 16:53:03 +0300

    Небольшие правки в random_numbers_generator.cs

--- random_numbers_generator.cs
+++ random_numbers_generator.cs
@@ -1,26 +1,25 @@
-// ыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыы
 namespace test1
 {
     class Program
     {
         static void Main()
         {
-            Console.Write("Ввелите количество рандомных чисел: ");
+            Console.Write("Введите количество рандомных чисел: ");
             short n = Convert.ToInt16(Console.ReadLine());
-            
+
             Console.Write("Введите минимальное рандомное число: ");
             short n2 = Convert.ToInt16(Console.ReadLine());
-            
+
             Console.Write("Введите максимальное рандомное число: ");
             short n3 = Convert.ToInt16(Console.ReadLine());
-            
+
             short[] numbers = new short[n];
             Random rand = new Random();
-            
-            for (int i = 1; i < numbers.Length; i++)
+
+            for (int i = 0; i < numbers.Length; i++)
             {
                 numbers[i] = Convert.ToInt16(rand.Next(n2, n3));
-                Console.WriteLine($"Число {i}: {numbers[i]}");
+                Console.WriteLine($"Число {i + 1}: {numbers[i]}");
             }
         }
     }
