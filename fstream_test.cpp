5ccb784dbc83ee2748c5ee2220fa599beb472a90
def <153857992+def0001@users.noreply.github.com>
2025-06-02 17:03:41 +0300

    Обновлён fstream_test.cpp

--- fstream_test.cpp
+++ fstream_test.cpp
@@ -4,12 +4,37 @@
 
 int writeFile(const char* path, const char* text);
 int readFile(const char* path);
+void eraseCharArray(char array[]);
 
 int main() {
-	writeFile("file.txt", "i like my cat!");
-	readFile("file.txt");
-	writeFile("file.txt", "my cat is beautyfull!");
-	readFile("file.txt");
+	char text[1000] = "";
+	char act[32] = "";
+	char path[100] = "";
+
+	while (true) {
+		std::cout << "(act)> ";
+		std::cin.getline(act, sizeof(act));
+
+		if (std::strcmp(act, "write") == 0) {
+			std::cout << "(text)> ";
+			std::cin.getline(text, sizeof(text));
+			std::cout << "(path)> ";
+			std::cin.getline(path, sizeof(path));
+			writeFile(path, text);
+		} else if (std::strcmp(act, "read") == 0) {
+			std::cout << "(path)> ";
+			std::cin.getline(path, sizeof(path));
+			readFile(path);
+		} else if (std::strcmp(act, "e") || std::strcmp(act, "exit")) {
+			return 0;
+		} else {
+			std::cerr << "Error: unknow action \"" << act << "\"!\n";
+		}
+
+		eraseCharArray(text);
+		eraseCharArray(act);
+		eraseCharArray(path);
+	}
 }
 
 int writeFile(const char* path, const char* text) {
@@ -42,4 +67,12 @@ int readFile(const char* path) {
 
 	file.close();
 	return 0;
+}
+
+void eraseCharArray(char array[]) {
+	int i = 0;
+	for (i = 0; i < sizeof(array); i++) {
+		array[i] = {};
+	}
+	i = {};
 }
